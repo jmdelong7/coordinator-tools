dda2da7d81c3fdc3da7ab658e82abb3052fc338e
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2024-10-27 14:06:30 -0700

    Remove propogate, DateCalculator updateValues
    updateValues to keep values upt to date when one is changed.
    getNextMonday & getPreviousMonday to easily find start dates.

--- src/date-calculator.js
+++ src/date-calculator.js
@@ -1,28 +1,82 @@
-import { add as addDate } from 'date-fns';
+import {
+  add as addDate, 
+  sub as subDate,
+  differenceInCalendarDays,
+  nextMonday,
+  previousMonday,
+} from 'date-fns';
 
 export default class DateCalculator {
   constructor(startDate) {
     this.startDate = startDate;
-    this.updateValues({ weeks: 4 });
+    this.updateValues({weeks: 4});
   }
 
-  updateValues({ weeks = null, days = null, periods = null}) {
-    if (weeks !== null) {
-      this.weeks = weeks;
-      this.days = weeks * 7;
-      this.periods = weeks / 4;
-      this.endDate = addDate(this.startDate, {weeks: weeks});  
+  #propogateDayDifference(endDate, startDate) {
+    const dayDifference = differenceInCalendarDays(endDate, startDate);
+    this.weeks = dayDifference / 7;
+    this.days = dayDifference;
+    this.periods = dayDifference / 28;
+  }
+
+  #updateStartDate(newStartDate) {
+    this.startDate = newStartDate;
+    this.endDate = addDate(this.startDate, {weeks: this.weeks});
+    this.#propogateDayDifference(this.endDate, newStartDate);
+  }
+
+  #updateEndDate(newEndDate) {
+    this.endDate = newEndDate;
+    this.startDate = subDate(this.endDate, {weeks: this.weeks});
+    this.#propogateDayDifference(newEndDate, this.startDate);
+  }
+
+  #updateWeeks(newWeeksValue) {
+    this.weeks = newWeeksValue;
+    this.endDate = addDate(this.startDate, {weeks: newWeeksValue});
+    this.#propogateDayDifference(this.endDate, this.startDate);
+  }
+
+  #updateDays(newDaysValue) {
+    this.days = newDaysValue;
+    this.endDate = addDate(this.startDate, {days: newDaysValue});
+    this.#propogateDayDifference(this.endDate, this.startDate);
+  }
+
+  #updatePeriods(newPeriodsValue) {
+    this.periods = newPeriodsValue;
+    this.endDate = addDate(this.startDate, {weeks: newPeriodsValue * 4});
+    this.#propogateDayDifference(this.endDate, this.startDate);
+  }
+
+  updateValues({startDate = null, endDate = null, weeks = null, days = null, periods = null}) {
+    if (startDate !== null) {
+      this.#updateStartDate(startDate);
+    } else if (endDate !== null) {
+      this.#updateEndDate(endDate);
+    } else if (weeks !== null) {
+      this.#updateWeeks(weeks);
     } else if (days !== null) {
-      this.days = days;
-      this.weeks = days / 7;
-      this.periods = days / 28;
-      this.endDate = addDate(this.startDate, {days: days});
+      this.#updateDays(days);
     } else if (periods !== null) {
-      this.periods = periods;
-      this.weeks = periods * 4;
-      this.days = periods * 28;
-      this.endDate = addDate(this.startDate, {weeks: this.weeks});
+      this.#updatePeriods(periods);
+    }
+    this.endDate = subDate(this.endDate, ({days: 1}));
+    if (this.days <= 0) {
+      this.endDate = null;
+      this.weeks = null;
+      this.days = null;
+      this.periods = null;
     }
   }
 
+  getNextMonday(date) {
+    const monday = nextMonday(date);
+    this.updateValues({startDate: monday});
+  }
+
+  getPreviousMonday(date) {
+    const monday = previousMonday(date);
+    this.updateValues({startDate: monday});
+  }
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import DateCalculator from "./date-calculator";
 
 const sd = new Date(2024, 11, 30);
 
-const dateCalculator = new DateCalculator(sd)
+const dateCalculator = new DateCalculator(sd);
 window.dateCalculator = dateCalculator;
