2f025252db7bc1aa1ee667ad8bb3cbac71d139d5
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2024-11-17 15:07:57 -0800

    .date-start & .date-end width

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dateController from "./date-calculator";
 import './styles.css';
 
-const startDate = '2024-12-02';
+const startDate = '2024-09-23';
 const dateInterface = dateController(startDate);
 window.dateInterface = dateInterface;
