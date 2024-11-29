060af0476940710eb572f1f669227ddfb53b5fbe
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2024-11-29 11:05:55 -0800

    CpmDisplay showValueError

--- src/cpm-calculator.js
+++ src/cpm-calculator.js
@@ -21,33 +21,50 @@ class CpmDisplay {
     this.impressions = document.getElementById('impressions');
     this.calculate = document.getElementById('calculate');
     this.clear = document.getElementById('clear');
-
     [this.budget, this.cpm, this.impressions].map((input) => input.value = 0);
 
     this.addListeners();
   }
 
-  calculateMissingValue() {
-    const currentValues = {
+  get metricPairs() {
+    return {
       budget: Number(this.budget.value),
       cpm: Number(this.cpm.value),
       impressions: Number(this.impressions.value)
     };
+  }
 
-    const keys = Object.keys(currentValues);
+  showValueError(show) { 
+    const valueError = document.getElementById('cpm-value-error');
+    if (show === true) {
+      valueError.classList.add('show');
+    } else if (show === false) {
+      valueError.classList.remove('show');
+    }
+  } 
+
+  calculateMissingValue() {
+    const keys = Object.keys(this.metricPairs);
     const givenValues = Object.fromEntries(
-      keys.filter((key) => currentValues[key] !== 0)
-      .map((key) => [key, currentValues[key]])
+      keys.filter((key) => this.metricPairs[key] !== 0)
+      .map((key) => [key, this.metricPairs[key]])
     );
-
-    return cpmCalculator(givenValues);
+    if (Object.keys(givenValues).length === 2) {
+      this.showValueError(false);
+      return cpmCalculator(givenValues);
+    } else {
+      this.showValueError(true);
+      return false;
+    }
   }
 
   updateValues() {
     const newValues = this.calculateMissingValue();
-    this.budget.value = newValues.budget;
-    this.cpm.value = newValues.cpm;
-    this.impressions.value = newValues.impressions;
+    if (newValues) {
+      this.budget.value = newValues.budget;
+      this.cpm.value = newValues.cpm;
+      this.impressions.value = newValues.impressions;  
+    }
   }
 
   addListeners() {
@@ -55,6 +72,7 @@ class CpmDisplay {
       this.updateValues();
     });
     this.clear.addEventListener('click', () => {
+      this.showValueError(false);
       this.budget.value = 0;
       this.cpm.value = 0;
       this.impressions.value = 0;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import dateController from "./date-calculator";
 import cpmController from "./cpm-calculator";
 import './styles/main.css';
 import './styles/date-calculator.css';
+import './styles/cpm-calculator.css';
 
 const startDate = '2024-09-23';
 const dateInterface = dateController(startDate);
