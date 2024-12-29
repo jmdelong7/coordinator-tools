ad79adadd1975bd729a93307d247ebf07e330a40
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2024-12-28 18:43:02 -0800

    Initial dist subtree commit

--- webpack.prod.js
+++
@@ -1,9 +0,0 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
-
-module.exports = merge(common, {
-  mode: 'production',
-  output: {
-    publicPath: '/coordinator-tools/',  // Add this line - must match your repo name
-  }
-});
